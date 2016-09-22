f0e712b46e031900cd83ecc28b854a2ff5522250
William Liew <William Liew>
2016-09-22 23:28:57 +0800

    Update raw folder with d to y figures
    No j and z due to asl requires motion

---
+++ raw/getRaw.m
@@ -0,0 +1,30 @@
+
+%% Kinect adaptor and devices
+info = imaqhwinfo('kinect');
+
+%% create videoinput Object for Color Stream
+% info.DeviceInfo(1)
+% colorVid = videoinput('kinect',1,'RGB_640x480');
+% preview(colorVid);
+% 
+% RGBImage = getsnapshot(colorVid);
+% imshow(RGBImage);
+
+%% Videoinput Object for Depth Stream
+info.DeviceInfo(2)
+depthVid = videoinput ('kinect',2,'Depth_640x480');
+preview(depthVid);
+
+i = 0;
+for k = 'z'
+    for i = 1:4
+    input('Press Enter to proceed\n');
+    FileName = [k int2str(i)];
+    fprintf(1, 'Capturing %s in 2 seconds\n', FileName);
+    pause(2);
+    depthImage = getsnapshot(depthVid);
+    imshow(depthImage, [0 4000])
+    savefig(FileName);
+    fprintf(1, 'Saving figure: %s\n', FileName);
+    end
+end
