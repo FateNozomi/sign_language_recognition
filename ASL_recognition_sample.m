272cf511bb8851918a86f878d5c7257c84799e7a
L <L>
2016-11-16 22:49:29 +0800

    Update to use database_v2_2

--- ASL_recognition_sample.m
+++ ASL_recognition_sample.m
@@ -3,31 +3,34 @@
 % Call getDatabase_v2_1 if database does not exist
 if ~exist('database','var')
     % get 2-D array database
-    database = getDatabase_v2_1;
+    database = getDatabase_v2_2;
 end
 
-% compute the mean of each column in database
-m = mean(database,1);
-mDatabase = double(m);
+% Get databaseState and databaseState3
+databaseOpen = database{1};
+databaseClosed = database{2};
 
-% Get the number of columns in database
-cDatabase = size(database);
-cDatabase = cDatabase(2);
+% Get the number of columns in both database
+colDatabaseOpen = size(databaseOpen);
+colDatabaseOpen = colDatabaseOpen(2);
+colDatabaseClosed = size(databaseClosed);
+colDatabaseClosed = colDatabaseClosed(2);
 
-databaseAdjust = [];
-for i = 1:cDatabase
-    temp = double(database(:,i))-mDatabase(i);
-    databaseAdjust = [databaseAdjust temp];
-end
+% PCA on both database
+[coeff1,score1] = pca(double(databaseOpen));
+[coeff2,score2] = pca(double(databaseClosed));
 
-[coeff,score] = pca(double(database));
+Xcentered1 = score1*coeff1';
+PCA_DatabaseOpen = score1'*Xcentered1;
 
-Xcentered = score*coeff';
-W = score'*Xcentered;
+Xcentered2 = score2*coeff2';
+PCA_DatabaseClosed = score2'*Xcentered2;
+
 
 % Test unknown sample image
-unknownSample = fullfile([pwd '\sample_v2_1'], 't11.fig');
+unknownSample = fullfile([pwd '\sample_v2'], 'y11.fig');
 fprintf(1, '\nIdentifiying unknown sample %s\n', unknownSample);
+HandRightState = 1;
 imTemp1 = openfig(unknownSample,'invisible');
 imTemp2 = findobj(imTemp1,'type','image');
 I = imTemp2.CData;
@@ -47,7 +50,11 @@ I2((I2>90)) = 4000;
 
 I = I2;
 
-Z = cropImage_v2_1(I);
+if HandRightState == 3
+    Z = cropImage_v2_1(I);
+else
+    Z = cropImage_v2(I);
+end
 
 figure;imshow(Z, [0 100]);
 
@@ -57,22 +64,45 @@ Z=reshape(Z,irow*icol,1);
 Z=double(Z);
 Z=Z-mean(Z);
 
-score2 = score'*Z;
-
-for i = 1:cDatabase
-    difference(i) = sum(abs(score2-W(:,i)));
+if HandRightState == 3
+    PCA_DatabaseClosedUnknown = score2'*Z;
+    
+    for i = 1:colDatabaseClosed
+        difference(i) = sum(abs(PCA_DatabaseClosedUnknown-PCA_DatabaseClosed(:,i)));
+    end
+    
+    finalDiff=[];
+    for i = 1:10:colDatabaseClosed
+        temp2 = sum(difference(:,i:i+9));
+        finalDiff = [finalDiff temp2];
+    end
+    
+    finalDiff
+    
+    minDiffPos = find(finalDiff==min(finalDiff));
+    
+    alphabetList = ['A' 'E' 'M' 'N' 'S' 'T'];
+    minDiff = finalDiff(:,minDiffPos);
+    alphabetSign = alphabetList(:,minDiffPos)
+    
+else
+    PCA_DatabaseOpenUnknown = score1'*Z;
+    
+    for i = 1:colDatabaseOpen
+        difference(i) = sum(abs(PCA_DatabaseOpenUnknown-PCA_DatabaseOpen(:,i)));
+    end
+    
+    finalDiff=[];
+    for i = 1:10:colDatabaseOpen
+        temp2 = sum(difference(:,i:i+9));
+        finalDiff = [finalDiff temp2];
+    end
+    
+    finalDiff
+    
+    minDiffPos = find(finalDiff==min(finalDiff));
+    
+    alphabetList = ['B':'D' 'F':'I' 'K':'L' 'O':'R' 'U':'Y'];
+    minDiff = finalDiff(:,minDiffPos);
+    alphabetSign = alphabetList(:,minDiffPos)
 end
-
-finalDiff=[];
-for i = 1:10:cDatabase;
-    temp2 = sum(difference(:,i:i+9));
-    finalDiff = [finalDiff temp2];
-end
-
-finalDiff
-
-minDiffPos = find(finalDiff==min(finalDiff));
-
-alphabetList = ['A' 'E' 'M' 'N' 'S' 'T'];
-minDiff = finalDiff(:,minDiffPos);
-alphabetSign = alphabetList(:,minDiffPos)
